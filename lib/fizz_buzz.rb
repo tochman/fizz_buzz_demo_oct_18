0aeda90d9da2b529fad129b0796e6a490cbff06c
Thomas <thomas@craftacademy.se>
2018-10-18 16:49:04 +0200

    adds more specs (for number 5 and 15)

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,11 @@
 class FizzBuzz
   def check(number)
-    if number % 3 == 0 
+    if number % 15 == 0
+        return 'fizzbuzz'
+    elsif number % 3 == 0 
         return 'fizz'
+    elsif number % 5 == 0
+        return 'buzz'
     else
         return number
     end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe FizzBuzz do
         expect(subject.check(3)).to eq 'fizz'
     end
 
+    it 'returns buzz if number is divisible by 5' do 
+        expect(subject.check(5)).to eq 'buzz'
+    end
+
+    it 'returns fizzbuzz if number is divisible by 15' do 
+        expect(subject.check(15)).to eq 'fizzbuzz'
+    end
+
 end
