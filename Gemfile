3b1f6e82841fed48f96edd75ff8744646d19849f
Thomas <thomas@craftacademy.se>
2018-10-18 17:46:20 +0200

    another way of doing it....

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'rspec'
+gem 'numbers_in_words'
+gem 'pry'
--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,15 +1,46 @@
+require 'numbers_in_words'
+require 'numbers_in_words/duck_punch'
+require 'pry'
+
 class FizzBuzz
   def check(number)
-    if number.negative?
-        return "Are you for real? This is a negative number!"
-    elsif number % 15 == 0
-        return 'fizzbuzz'
-    elsif number % 3 == 0 
-        return 'fizz'
-    elsif number % 5 == 0
-        return 'buzz'
+    if number.is_not_suitable_for_fizz_buzz
+      'Are you for real? This is a negative number!'
+    elsif number.is_divisible_by_fifteen?; say_fizzbuzz
+    elsif number.is_divisible_by_three?; say_fizz
+    elsif number.is_divisible_by_five?; say_buzz
     else
-        return number
+      number
     end
+  end
+
+  protected
+
+  def say_something
+    method_source = __callee__
+    method_source.to_s.sub('say_', '')
+  end
+
+  %w[fizzbuzz fizz buzz].each do |message|
+    alias_method "say_#{message}", :say_something
+  end
+end
+
+class Integer
+  def method_missing(method, *_args)
+    if method.to_s.include? 'is_divisible'
+      new_method = method.to_s.sub('is_', '').to_sym
+      self.class.send(:define_method, new_method) do
+        divider = method.to_s.split('_').last.chomp('?').in_numbers
+        self % divider == 0
+      end
+      send(new_method)
+    end
+  end
+  end
+
+class Object
+  def is_not_suitable_for_fizz_buzz
+    !is_a?(Integer) || negative?
   end
 end
